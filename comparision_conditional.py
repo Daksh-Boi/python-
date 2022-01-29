defba3b1f17de10ce3f9f422473438db77c5ee07
Daksh-Boi <97043490+Daksh-Boi@users.noreply.github.com>
2022-01-29 19:13:28 +0530

    Add files via upload

---
+++ comparision_conditional.py
@@ -0,0 +1,22 @@
+
+x = int(input("Input the value of x "))
+y = int(input("Input the value of y "))
+if x>y:
+    print("X is greater than Y ")
+elif y>x :
+    print("Y is greater than X")
+else :
+    print("X is equal to Y")
+
+
+snow = int(input("Enter 1 if it snows or 0 if it doesn't "))
+if snow==1:
+    print("I will go out")
+else :
+    print("I will not go out")
+
+rain = int(input("Enter 1 if it is raining frogs or 0 if it isn't "))
+if rain==1:
+    print("I will dance")
+else :
+    print("I will not dance")
